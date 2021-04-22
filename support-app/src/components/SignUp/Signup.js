c0a6a87b3242e4b86b1c9edd05e2bbea6cf612f6
Christina Flores <cdflori1@gmail.com>
2021-04-21 22:40:01 -0700

    reworked form to function as onboarding wizard, replaced initial use state values to react hook form layouts.

--- support-app/src/components/SignUp/Signup.js
+++ support-app/src/components/SignUp/Signup.js
@@ -1,34 +1,37 @@
 import { useState } from 'react';
-import { Redirect, useHistory, useLocation } from 'react-router-dom';
+import { Redirect, useLocation } from 'react-router-dom';
 import useAuth from '../../hooks/auth';
-import {Form, Button} from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import { Form, Button } from 'react-bootstrap';
 import './style.css';
 
 const Signup = () => {
-    const { signup, isLoggedIn } = useAuth();
+    const { isLoggedIn } = useAuth();
     // History and location are hooks we can use to manipulate our page's history!
-    const history = useHistory();
+    // const history = useHistory();
     const location = useLocation();
-    const [userType, setUserType] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [EID, setEID] = useState('');
-    const [phone, setPhone] =('');
-    const [password, setPassword] = useState('');
-    const [confirmPass, setConfirmPass] = useState('');
+    const { watch, register, formState: { errors } } = useForm();
+    const [formStep, setFormStep] = useState(0)
+    // const [userType, setUserType] = useState('');
+    // const [firstName, setFirstName] = useState('');
+    // const [lastName, setLastName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [EID, setEID] = useState('');
+    // const [phone, setPhone] =('');
+    // const [password, setPassword] = useState('');
+    // const [confirmPass, setConfirmPass] = useState('');
     // For our redirector
     const [redirectToLogin, toggleRedirect] = useState(false);
     // This is the key part to our redirector. We can pull the prior location out here, if it exists
     const { from } = location.state || { from: { pathname: '/' } };
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        signup(email, password, userType, firstName, lastName, EID, confirmPass).then(res => {
-            // Go back to whence you came!
-            history.replace(from);
-        });
-    };
+    // const handleSubmit = event => {
+    //     event.preventDefault();
+    //     signup().then(res => {
+    //         // Go back to whence you came!
+    //         history.replace(from);
+    //     });
+    // };
 
     if (isLoggedIn()) {
         return <Redirect to={location.state || '/'} />;
@@ -43,9 +46,23 @@ const Signup = () => {
         />;
     }
 
-    // handleFormSelection() {
-    //     this.setState()
-    // }
+    const completeFormStep = () => {
+        setFormStep(cur => cur + 1)
+    }
+
+    const renderButton = () => {
+        if (formStep > 2) {
+            return undefined
+        } else if (formStep === 2){
+            return (
+                <Button onClick={completeFormStep} className='signupBtn' type='button'>Create Account</Button>
+            )
+        } else {
+            return (
+                <Button onClick={completeFormStep} className='signupBtn' type='button'>Next Step</Button>
+            )
+        }
+    }
 
     return (
         <div>
@@ -53,69 +70,68 @@ const Signup = () => {
                 Signup Page
             </h2>
             <div className='formContainer d-lg-flex justify-content-center'>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group>
-                    <Form.Label>Type</Form.Label>
-                    <Form.Control 
-                        as ="select" 
-                        name='userType'
-                        value={userType}
-                        onChange={event => setUserType(event.target.value)}>
-                        <option>Instructor</option>
-                        <option>Oil Rig User</option>
-                    </Form.Control>
-                </Form.Group>
+            <Form>
+                {formStep === 0 && (
+                    <Form.Group>
+                        <Form.Label>Type</Form.Label>
+                        <Form.Control 
+                            as ="select" 
+                            name='userType'>
+                            <option>Instructor</option>
+                            <option>Oil Rig User</option>
+                            ref={register({
+                                required: {
+                                    value: true,
+                                    message: 'Please select user type',
+                                },
+                            })}
+                        </Form.Control>
+                    </Form.Group>
+                )}
+                {formStep === 1 && (
                 <Form.Group>
                     <Form.Label htmlFor='first name'>First Name</Form.Label>
                     <Form.Control 
                         name='firstName' 
-                        type='firstName' 
+                        type='test' 
+                        id='firstName'
                         placeholder='First Name' 
-                        value={firstName} 
-                        onChange={event => setFirstName(event.target.value)}/>
+                        ref={register()}
+                    />
                     <br/>    
                     <Form.Label htmlFor='last name'>Last Name</Form.Label>    
                     <Form.Control 
                         name='lasttName' 
                         type='lastName' 
                         placeholder='Last Name' 
-                        value={lastName} 
-                        onChange={event => setLastName(event.target.value)}/>    
-                </Form.Group>
-                <Form.Group>
+                        />  
+                    <br/> 
                     <Form.Label htmlFor='email'>Email</Form.Label>
                     <Form.Control
                         name='email'
                         placeholder='Email'
                         type='email'
                         autoComplete='username'
-                        value={email}
-                        onChange={event => setEmail(event.target.value)}
-                    />
-                </Form.Group>   
-                <Form.Group>
+                    /> 
+                    <br/>
                     <Form.Label htmlFor='Employee ID'>Employee ID</Form.Label>
-                    <Form.Control
-                        name='EID'
-                        placeholder='####'
-                        type='EID'
-                        // autoComplete='username'
-                        value={EID}
-                        onChange={event => setEID(event.target.value)}
-                    />
-                </Form.Group> 
-                <Form.Group>
+                        <Form.Control
+                            name='EID'
+                            placeholder='####'
+                            type='EID'
+                        />
+                    <br/>
                     <Form.Label htmlFor='Phone Number'>Phone Number</Form.Label>
                     <Form.Control
                         name='phone'
                         placeholder='####'
                         type='phone'
-                        // autoComplete='username'
-                        value={phone}
-                        onChange={event => setPhone(event.target.value)}
                     />
-                </Form.Group> 
+                </Form.Group>
+                
+                )}   
                 <br />
+                {formStep === 2 && (
                 <Form.Group>
                 <Form.Label htmlFor='password'>Password</Form.Label>
                 <Form.Control
@@ -123,23 +139,26 @@ const Signup = () => {
                     placeholder='Password'
                     type='password'
                     autoComplete='password'
-                    value={password}
-                    onChange={event => setPassword(event.target.value)}
                 />
-                </Form.Group>
-                <Form.Group>
+                <br/>
                 <Form.Label htmlFor='confirm password'>Confirm Password</Form.Label>
                 <Form.Control
                     name='password'
                     placeholder='Password'
                     type='password'
                     autoComplete='password'
-                    value={confirmPass}
-                    onChange={event => setConfirmPass(event.target.value)}
                 />
                 </Form.Group>
+                )}
                 <br />
-                <Button className='signupBtn' type='submit'>Signup</Button>
+                {formStep === 3 && (
+                <h2>You've Created An Account!</h2>
+                )}
+                <br/>
+                {renderButton()}
+                <pre>
+                    {JSON.stringify(watch(), null, 2)}
+                </pre>
             </Form>
             </div>
             <p>
